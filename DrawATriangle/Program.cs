78846929e4405036eb00ccdb7e5e0845f3196842
Django <Django@WIN-KCF7436SS02>
2021-08-27 15:31:52 +0300

    Add project files.

---
+++ DrawATriangle/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace DrawATriangle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите высоту треугольника: ");
+            int height = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < height; i++)
+            {
+                for (int j = height; j > i; j--)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
